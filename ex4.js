6061aa969822708885dc9431cf4bbeec47ffc8f1
Thalles Lima <thalles.irado@hotmail.com>
2022-06-30 00:21:08 -0300

    first commit

---
+++ ex4.js
@@ -0,0 +1,5 @@
+/*
+os dois  estarão na mesma distância de Ribeirão Preto 
+independente de onde estejam os 2 estarão exatamente na mesma distância da cidade 
+
+*/
---
+++ ex5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int mainI(){
+  char texto[80], textoinv[80];
+  int i, j, fim;
+  fgets(texto, 80, stdin);
+  fim = 0;
+  
+  
+  while (texto[fim] !=  '/0' && texto[fim] != 'n/')
+  {
+     fim++;
+  }
+  
+i = fim-1;
+j=0;
+
+while (j < fim)
+{
+    textoinv[j] = texto[i];
+    i--;
+    j++;
+}
+
+textoinv[fim]= '/0';
+
+printf("inversa:  %s ", textoinv);
+
+}
