4bb0c348ff0c76daa1fa639789e10b63a27a3ced
Thalles Lima <thalles.irado@hotmail.com>
2022-07-05 20:20:45 -0300

    estagio desenv

--- ex4.js
+++ ex4.js
@@ -1,5 +1,4 @@
 /*
-os dois  estarão na mesma distância de Ribeirão Preto 
-independente de onde estejam os 2 estarão exatamente na mesma distância da cidade 
-
+ por conta da velocidade O carro (que está mais rapido) vai conseguir percorrer um trajeto maior do que o caminhão até eles se encontrarem
+ além dos pedágios q o caminhão fica pra trás e o carro n, fazendo ele percorrer mais distancia
 */
